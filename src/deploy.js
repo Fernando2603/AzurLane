e987dc23795ddf21bcf50aaaaa94ddc9ba4ae91f
Fernando <81207411+Fernando2603@users.noreply.github.com>
2022-06-18 09:31:02 +0000

    [18062022]

---
+++ src/deploy.js
@@ -0,0 +1,12 @@
+import fs from "node:fs";
+
+const ShipBanner = JSON.parse(fs.readFileSync("./src/ShipBanner.json"));
+
+fs.writeFile("./ShipBanner.json", JSON.stringify(ShipBanner, null, "\t"), 'utf8', function (err) {
+    if (err)
+    {
+        console.log("An error occured while writing JSON to File");
+        return console.log(err);
+    };
+    console.log("=> ./ShipBanner.json has been updated!");
+});
